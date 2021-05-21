b949245989bead64aec7ca40a1700bf98d6d4cc7
Vamsi Koneti <kavamsikrishna25800@gmail.com>
2021-05-21 11:54:52 +0530

    Added Grape

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ import pygame
 import time
 from pygame.locals import *
 
+#grape has dimensions 5x5
+
+class Grape:
+    def __init__ (self, parentScreen):
+        self.grape = pygame.image.load("resources/grape.png").convert()
+        self.parentScreen = parentScreen
+        self.x = SIZE * 10
+        self.y = SIZE *10
+
+    def draw(self):
+        self.parentScreen.blit(self.grape, (self.x , self.y))
+
+        pygame.display.flip()
+
+
+
+SIZE = 35
 class Snake:
-    def __init__(self, parentScreen):
+    def __init__(self, parentScreen, length):
+        self.length = length
         self.parentScreen = parentScreen
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE] * length
+        self.y = [SIZE] * length
         self.direction = 'down'
 
     def draw(self):
         self.parentScreen.fill((230, 115, 80))
 
-        #draw image as background
-        self.parentScreen.blit(self.block, (self.x , self.y))
+        for i in range (self.length):
+            self.parentScreen.blit(self.block, (self.x[i] , self.y[i]))
 
         pygame.display.flip()
 
@@ -31,15 +49,21 @@ class Snake:
         self.direction = 'down'
 
     def walk(self):
+        for i in range(self.length-1, 0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+
+
         if self.direction == 'up':
-            self.y -= 10
+            self.y[0] -= SIZE
         if self.direction == 'down':
-            self.y +=10
+            self.y[0] +=SIZE
 
         if self.direction == 'left':
-            self.x -=10
+            self.x[0] -= SIZE
         if self.direction == 'right':
-            self.x +=10
+            self.x[0] += SIZE
         self.draw()
 
 
@@ -47,14 +71,23 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
+        length = 7
 
         #initialize pygame window
-        self.surface = pygame.display.set_mode((1000, 1000))
+        self.surface = pygame.display.set_mode((750,750))
         self.surface.fill((230, 115, 80))
 
         #snake class is inside the Game() class
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, length)
         self.snake.draw()
+
+        #initialize grape
+        self.grape = Grape(self.surface)
+        self.grape.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.grape.draw()
 
 
     def run(self):
@@ -84,7 +117,7 @@ class Game:
                 elif (ev.type == QUIT) :
                     run = False
 
-            self.snake.walk()
+            self.play()
             #timer to move snake automatically
             time.sleep(0.2)
 
