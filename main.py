44f4eec77b49d03206601854a13df54f4112bbb9
Vamsi Koneti <kavamsikrishna25800@gmail.com>
2021-05-21 18:45:01 +0530

    Added game over logic

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from pygame.locals import *
 import random
 
 #grape has dimensions 5x5
+BACKGROUND = (230, 115, 80)
+SIZE = 35
+
+
 
 class Grape:
     def __init__ (self, parentScreen):
@@ -21,7 +25,6 @@ class Grape:
         self.x = random.randint(1,20)*SIZE
         self.y = random.randint(1,20)*SIZE
 
-SIZE = 35
 class Snake:
     def __init__(self, parentScreen, length):
         self.length = length
@@ -32,7 +35,7 @@ class Snake:
         self.direction = 'down'
 
     def draw(self):
-        self.parentScreen.fill((230, 115, 80))
+        self.parentScreen.fill(BACKGROUND)
 
         for i in range (self.length):
             self.parentScreen.blit(self.block, (self.x[i] , self.y[i]))
@@ -85,7 +88,7 @@ class Game:
 
         #initialize pygame window
         self.surface = pygame.display.set_mode((750,750))
-        self.surface.fill((230, 115, 80))
+        self.surface.fill(BACKGROUND)
 
         #snake class is inside the Game() class
         self.snake = Snake(self.surface, length)
@@ -108,43 +111,72 @@ class Game:
         self.displayScore()
         pygame.display.flip()
 
+        #snake eating grape
         if self.collision(self.snake.x[0], self.snake.y[0], self.grape.x, self.grape.y):
             self.snake.increaseLength()
             self.grape.move()
 
+        #snake colliding with itself
+        for i in range(3, self.snake.length):
+            if self.collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise "Game Over"
+
     def displayScore(self):
         font = pygame.font.SysFont('arial', 30)
-        score = font.render(f"Score:  {self.snake.length}", True, (0,0,0))
-        self.surface.blit(score, (800,10))
+        score = font.render(f"Score:  {self.snake.length}", True, (255,255,255))
+        self.surface.blit(score, (600,10))
+    
+    def gameOver(self):
+        font = pygame.font.SysFont('arial', 30)
+        self.surface.fill(BACKGROUND)
+        line1 = font.render(f"Game Over! Your score is: {self.snake.length}", True, (255,255,255))
+        self.surface.blit(line1, (200,300))
+        line2 = font.render('Press Enter to replay!', True, (255,255,255))
+        self.surface.blit(line2, (200,350))
+        pygame.display.flip()
+
+    def reset(self):
+        self.snake = Snake(self.surface, 1)
+        self.grape = Grape(self.surface)
 
     def run(self):
         #event loop
         run = True
+        pause = False
         while run:
             for ev in pygame.event.get():
 
                 if ev.type == KEYDOWN:
-
+                    
+                    if (ev.key == K_RETURN):
+                        pause = False
                     #escape to quit
                     if(ev.key == K_ESCAPE):
                         run = False
 
-                    if(ev.key == K_UP):
-                        self.snake.moveUp()
+                    if not pause: 
+                        if(ev.key == K_UP):
+                            self.snake.moveUp()
 
-                    if(ev.key == K_DOWN):
-                        self.snake.moveDown()
+                        if(ev.key == K_DOWN):
+                            self.snake.moveDown()
 
-                    if(ev.key == K_LEFT):
-                        self.snake.moveLeft()
-                        
-                    if(ev.key == K_RIGHT):
-                        self.snake.moveRight()
+                        if(ev.key == K_LEFT):
+                            self.snake.moveLeft()
+                            
+                        if(ev.key == K_RIGHT):
+                            self.snake.moveRight()
                     
                 elif (ev.type == QUIT) :
                     run = False
 
-            self.play()
+            try:
+                if not pause:
+                    self.play()
+            except Exception as e:
+                self.gameOver()
+                pause = True
+                self.reset()
             #timer to move snake automatically
             time.sleep(0.2)
 
